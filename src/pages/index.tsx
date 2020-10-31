c5592e73d06b099e8111d9eb717c03e2d92031f7
Brian Chung <35940686+BrChung@users.noreply.github.com>
2020-10-31 15:06:37 -0700

    🚀 Initial Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
 import React from "react"
+import "../styles/index.scss"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <main className="main-container">
+      <section id="landing">
+        <span>This is the landing section</span>
+      </section>
+      <section id="about">
+        <span>This is the about section</span>
+      </section>
+      <section id="work">
+        <span>This is the work section</span>
+      </section>
+      <section id="contact">
+        <span>This is the contact section</span>
+      </section>
+    </main>
+  )
 }
