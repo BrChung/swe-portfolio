b6743da49ce1dc531220eeb72fe5f81ab975b2fb
Brian Chung <35940686+BrChung@users.noreply.github.com>
2020-10-31 15:28:10 -0700

    🚀 Initial Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,25 @@ export default function Home() {
         <span>This is the landing section</span>
       </section>
       <section id="about">
-        <span>This is the about section</span>
+        <div className="about-image-container"></div>
+        <div className="about-text-container">
+          <h2>
+            <span className="accent">1.&nbsp;</span>About Me
+          </h2>
+          <p>
+            Hello! I’m Michelle, a software engineer based in Fullerton, CA.
+          </p>
+          <p>
+            I am outgoing, dedicated, and open-minded. I get across to people
+            and adjust to changes with ease. I believe that a person should
+            develop their professional skills and learn new things all the time.
+            Currently, I am looking for new career opportunities to expand my
+            horizon.
+          </p>
+        </div>
+        <div className="about-stylish-text">
+          <span>ABOUT ME</span>
+        </div>
       </section>
       <section id="work">
         <span>This is the work section</span>
